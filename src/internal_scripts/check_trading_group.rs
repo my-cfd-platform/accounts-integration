ef3a32985d66ca965ffc6072278548ba29e85c54
amigin <amigin@gmail.com>
2024-04-27 14:20:41 +0200

    Added debugging of registration of trading groups

--- src/internal_scripts/check_trading_group.rs
+++ src/internal_scripts/check_trading_group.rs
@@ -15,6 +15,27 @@ pub async fn check_trading_group(
         .await
         .is_some()
     {
+        println!("Requested trading group: {}", trading_group_id);
+
+        let groups = app
+            .trading_groups
+            .get_by_partition_key(TradingGroupNoSqlEntity::generate_partition_key())
+            .await;
+
+        match groups {
+            Some(entities) => {
+                for entity in entities.values() {
+                    println!(
+                        "Entity in dictionary: PK:{}, RK:{}",
+                        entity.partition_key, entity.row_key
+                    );
+                }
+            }
+            None => {
+                println!("No trading groups in dictionary");
+            }
+        }
+
         return Err(AccountsIntegrationOperationResult::InvalidTradingGroup);
     }
 
